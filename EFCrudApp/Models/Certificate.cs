1e251c72d465ead8d0db08f41d156603213aef5e
Yun Jang <jang.yunj7@gmail.com>
2023-06-15 23:54:34 -0400

    added properties

---
+++ EFCrudApp/Models/Certificate.cs
@@ -0,0 +1,14 @@
+﻿namespace EFCrudApp.Models
+{
+    public class Certificate
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Scope { get; set; }
+        public string Language { get; set; }
+        public DateTime Date { get; set; }
+        public string Website { get; set; }
+        public string Authenticity { get; set; }
+    }
+}
